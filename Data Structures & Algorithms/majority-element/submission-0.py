df9480610ed03e46812ad706a87aea08227c2d73
Fidelis Mayaba Cheelo II <141682134+Fidelis-Mayaba-II-Cheelo@users.noreply.github.com>
2026-06-23 18:19:40 +0200

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,41 @@
+from collections import Counter
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        sorted_array = self.mergeSort(nums)
+        d = Counter(sorted_array)
+        most_common = d.most_common()
+        item, count = most_common[0]
+        return item
+
+
+    def mergeSort(self, nums: List[int]) -> List[int]:
+        if len(nums) < 2:
+            return nums
+
+        mid = len(nums) // 2
+        nums1, nums2 = nums[:mid], nums[mid:]
+
+        left_sorted = self.mergeSort(nums1)
+        right_sorted = self.mergeSort(nums2)
+
+        return self.merge(left_sorted, right_sorted)
+
+
+    def merge(self, first: List[int], second: List[int]) -> List[int]:
+        final = []
+        i, j = 0, 0
+
+        while i < len(first) and j < len(second):
+            if first[i] < second[j]:
+                final.append(first[i])
+                i += 1
+            else:
+                final.append(second[j])
+                j += 1
+        
+        final.extend(first[i:])
+        final.extend(second[j:])
+        return final
+
+        
