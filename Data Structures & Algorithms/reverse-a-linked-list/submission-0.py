1a7e70826a5077a5c9ec0e128fc1a73a73938e02
Fidelis Mayaba Cheelo II <141682134+Fidelis-Mayaba-II-Cheelo@users.noreply.github.com>
2026-07-01 13:45:12 +0200

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+
+class Node:
+    def __init__(self, val:Any):
+        self.val = val
+        self.next: Node | None = None
+    
+    def set_next(self, node: Node | None):
+        self.next = node
+
+    def __repr__(self):
+        return str(self.val)
+
+
+class Solution:
+    def reverseList(self, head: Optional[Node]) -> Optional[Node]:
+        current = head
+        previous = None
+
+        while current is not None:
+            next_node = current.next
+
+            #Reverse the pointer
+            current.next = previous
+
+            previous = current
+
+            current = next_node
+
+        return previous
+
+        
+
+             
+
+
+
+
+
+        
